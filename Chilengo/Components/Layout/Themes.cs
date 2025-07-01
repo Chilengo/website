2afc57772e1ce0346fcb78960c5f27a3ac072a68
Ben Burge <benburgecode@gmail.com>
2025-07-01 15:30:18 -0600

    Initial Project

---
+++ Chilengo/Components/Layout/Themes.cs
@@ -0,0 +1,12 @@
+﻿namespace Chilengo.Components.Layout;
+
+public static class Themes
+{
+    public static readonly MudTheme Default = new MudTheme()
+    {
+        PaletteLight = new PaletteLight()
+        {
+            
+        }
+    };
+}
